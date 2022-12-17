6afa83a7512c14da06901cc3c9c8d83dc96a1446
LiamTran99 <120779292+LiamTran99@users.noreply.github.com>
2022-12-17 16:31:46 +0700

    Add files via upload

---
+++ events.js
@@ -0,0 +1,43 @@
+const allEvents = [
+    {
+        "day": "01.01.2023",
+        "events": [
+            "Dzień ustawowo wolny od pracy",
+            "Remanent początkowy"
+        ]
+    },
+    {
+        "day": "06.01.2023",
+        "events": [
+            "Dzień ustawowo wolny od pracy"
+        ]
+    },
+    {
+        "day": "10.01.2023",
+        "events": [
+            "złożenie zgłoszenia INTRASTAT za grudzień 2022 r"
+        ]
+    },
+    {
+        "day": "16.01.2023",
+        "events": [
+            "ZUS (spółki akcyjne, spółki z o.o., spółdzielnie, stowarzyszenia, fundacje)",
+            "złożenie deklaracji na podatek leśny i podatek rolny na dany rok podatkowy - osoby prawne, jednostki organizacyjne oraz spółki nieposiadające osobowości prawnej",
+            "wpłata do PPK"
+
+        ]
+    },
+    {
+        "day": "20.01.2023",
+        "events": [
+            "wpłata za grudzień 2022 r. pobranych zaliczek Podatek dochodowy lub zryczałtowanego podatku dochodowego",
+            "wpłata za grudzień 2022 r. pobranych zaliczek PIT 4",
+            "wpłata za grudzień 2022 r. pobranych zaliczek PIT 8A",
+            "ZUS (jednoosobowa działalność gospodarcza, spółki: cywilne, jawne, partnerskie, kamandytowe, komandytowo-akcyjne)",
+            "wpłata za grudzień 2022 r. na PFRON oraz złożenie deklaracji",
+            "wpłata za grudzień 2022 r ryczałtu od przychodów ewidencjonowanych",
+            "złożenie deklaracji PIT-6 do wymiaru zaliczek podatku dochodowego od dochodów z działów specjalnych produkcji rolnej ustalanych przy zastosowaniu norm szacunkowych za 2022 r.",
+            "złożenie zawiadomienia o rezygnacji z opodatkowania w formie karty podatkowej (w razie wyboru skali podatkowej), a w przypadku przejścia z karty podatkowej na inne formy opodatkowania złożenie oświadczenia o wyborze podatku liniowego lub ryczałtu ewidencjonowanego"
+        ]
+    }
+];
